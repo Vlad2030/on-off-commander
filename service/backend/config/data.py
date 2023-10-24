6b32080809fcec9101cc0e496670222abd46bf28
vladdd0 <vlad123123v@gmail.com>
2023-10-24 17:55:07 +0300

    Update data.py

--- service/backend/config/data.py
+++ service/backend/config/data.py
@@ -6,18 +6,18 @@ class App(enum.Enum):
     def __str__(self) -> str:
         return self.value
 
-    TITLE = Environs.get("APP_TITLE")
-    VERSION = Environs.get("APP_VERSION")
+    TITLE = Environs.get("TITLE")
+    VERSION = Environs.get("VERSION")
     DOCS_ENABLED = Environs.get("DOCS_ENABLED")
-    DOCS_URL = Environs.get("APP_DOCS_URL")
+    DOCS_URL = Environs.get("DOCS_URL")
 
 
 class Database(enum.Enum):
     def __str__(self) -> str:
         return self.value
 
-    HOST = Environs.get("POSTGRES_CONTAINER_HOST")
-    PORT = Environs.get("POSTGRES_CONTAINER_PORT")
-    DB = Environs.get("POSTGRES_DB")
+    HOST = Environs.get("POSTGRES_HOST")
+    PORT = Environs.get("POSTGRES_PORT")
+    DB = Environs.get("POSTGRES_DATABASE")
     USER = Environs.get("POSTGRES_USER")
     PASSWORD = Environs.get("POSTGRES_PASSWORD")
