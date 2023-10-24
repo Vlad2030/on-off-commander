fecd39ababdd4c651e0e4f77dc3e2f74ed4d593e
vladdd0 <vlad123123v@gmail.com>
2023-10-24 17:43:54 +0300

    Update Computers db model

--- service/backend/models/computers.py
+++ service/backend/models/computers.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ARRAY
 from core.database import Base
 
 
@@ -7,7 +7,8 @@ class ComputersDatabase(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=True)
-    device_name = Column(String, nullable=False)
+    device_hostname = Column(String, nullable=False)
     device_platform = Column(String, nullable=False)
-    device_ip = Column(String, nullable=False)
+    device_ipv4 = Column(ARRAY(String), nullable=False)
+    device_ipv6 = Column(ARRAY(String), nullable=False)
     device_online = Column(Boolean, nullable=False)
