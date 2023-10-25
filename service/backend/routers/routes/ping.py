bfea49a86da0d9db80b825e0f0e3a10b6c281470
vladdd0 <vlad123123v@gmail.com>
2023-10-25 08:57:56 +0300

    Update ping.py

--- service/backend/routers/routes/ping.py
+++ service/backend/routers/routes/ping.py
@@ -1,20 +1,20 @@
 from fastapi import APIRouter, status
-from fastapi.responses import JSONResponse
-from schemas.responses.ping import Ping
+from fastapi.responses import ORJSONResponse
+from schemas.responses.ping import PingResponse
 
 router = APIRouter(prefix="/ping")
 
 @router.get(
     path="/",
-    response_model=Ping,
+    response_model=PingResponse,
     status_code=status.HTTP_200_OK,
     summary="Server health check",
     description="Ping endpoint",
-    response_description="""If "ok": true, then server is running successfully""",
+    response_description="If 'ok': true, then server is running successfully",
 )
-async def get_ping() -> JSONResponse:
-    ping = Ping(ok=True)
-    return JSONResponse(
+async def get_ping() -> ORJSONResponse:
+    ping = PingResponse(ok=True)
+    return ORJSONResponse(
         content=ping.dict(),
         status_code=status.HTTP_200_OK,
         )
